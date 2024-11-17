af6c9d93c3a4ed8b71653cd9fec82792898ce44e
Pranav Pillai <your-email@example.com>
2024-11-16 22:57:40 -0600

    Modified Homepage Changes

--- pnc-merger/src/Pages/Home/Home.js
+++ pnc-merger/src/Pages/Home/Home.js
@@ -1,13 +1,30 @@
 import { Link } from "react-router-dom";
+import "./Home.css";
+
 
 function Home() {
     return (
-        <>
-            <h1>this is home page</h1>
-            <Link to="/Sim">Create Sim</Link>
-        </>
+        <div className="home-container">
+            {/* Dark Orange Navbar */}
+            <header className="navbar">
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/Sim" className="nav-link">Simulation</Link>
+            </header>
 
-    )
+
+            {/* Main Content */}
+            <main className="home-content">
+                <h1>[PROJECT NAME]</h1>
+                <p>Simulate Database Merging</p>
+                <Link to="/Sim">
+                    <button className="start-btn">Begin Sim</button>
+                </Link>
+            </main>
+        </div>
+    );
 }
 
+
 export default Home;
+
+
