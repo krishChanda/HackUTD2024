9613d81df1b9a8e6c403c5d7c0bd58bd4378a71d
Krish Chanda <krish.chanda@gmail.com>
2024-11-16 23:26:38 -0600

    modifed sim page

--- pnc-merger/src/Pages/Simulation/Simulation.js
+++ pnc-merger/src/Pages/Simulation/Simulation.js
@@ -2,6 +2,12 @@ import React from "react";
 import "./Simulation.css";
 
 function Simulation() {
+    // Mock data representing uploaded databases
+    const databases = {
+        "Database A": ["Table A", "Table B", "Table C", "Table D", "Table E"],
+        "Database B": ["Table A", "Table B", "Table C"],
+    };
+
     return (
         <div className="simulation-container">
             <div className="header">
@@ -10,15 +16,41 @@ function Simulation() {
             </div>
             <div className="main-content">
                 <div className="sidebar">
-                    <button>
-                        Main Database <span>+</span>
-                    </button>
-                    <button>
-                        Merge Database <span>+</span>
-                    </button>
+                    {Object.keys(databases).map((dbName, index) => (
+                        <div key={index} className="database-selection">
+                            <div className="database-header">
+                                <span>{dbName}</span>
+                                <button className="collapse-button">-</button>
+                            </div>
+                            <div className="table-list">
+                                {databases[dbName].map((table, idx) => (
+                                    <div key={idx} className="table-item">
+                                        <label>
+                                            <input type="checkbox" defaultChecked /> {table}
+                                        </label>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                    <button className="run-sim-button">RUN SIM</button>
                 </div>
                 <div className="database-view">
-                    DATABASE VIEW
+                    <h2>DATABASE VIEW</h2>
+                    <div className="dynamic-grid-container">
+                        {Object.keys(databases).map((dbName, index) => (
+                            <div key={index} className="database-column">
+                                <h3>{dbName}</h3>
+                                <div className="card-grid">
+                                    {databases[dbName].map((table, idx) => (
+                                        <div key={idx} className="card">
+                                            {table}
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
             <div className="sim-results-container">
@@ -31,3 +63,6 @@ function Simulation() {
 }
 
 export default Simulation;
+
+
+
