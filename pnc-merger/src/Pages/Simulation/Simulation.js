d27e7fc1e75d94eb7eee3f26f93ec069c7bd9aca
Krish Chanda <krish.chanda@gmail.com>
2024-11-16 22:35:03 -0600

    modifed simulation page

--- pnc-merger/src/Pages/Simulation/Simulation.js
+++ pnc-merger/src/Pages/Simulation/Simulation.js
@@ -1,9 +1,33 @@
+import React from "react";
+import "./Simulation.css";
+
 function Simulation() {
     return (
-        <>
-            <h1>this is Simulation page</h1>
-        </>
-    )
+        <div className="simulation-container">
+            <div className="header">
+                <span>Simulation</span>
+                <span className="project-name">PROJECT NAME</span>
+            </div>
+            <div className="main-content">
+                <div className="sidebar">
+                    <button>
+                        Main Database <span>+</span>
+                    </button>
+                    <button>
+                        Merge Database <span>+</span>
+                    </button>
+                </div>
+                <div className="database-view">
+                    DATABASE VIEW
+                </div>
+            </div>
+            <div className="sim-results-container">
+                <div className="sim-results">
+                    SIM RESULTS
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Simulation;
